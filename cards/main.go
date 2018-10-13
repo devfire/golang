f5272f5fa6e7c1532376dae31975ceac908e9169
Igor <igor@devfire.io>
2018-10-13 13:35:13 -0400

    added slice

--- cards/main.go
+++ cards/main.go
@@ -5,6 +5,16 @@ import (
 )
 
 func main() {
-	var card = "Ace of Spades"
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
