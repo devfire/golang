cb6c32288485c83d199cb8cccafb82407eb7b610
Igor <igor@devfire.io>
2018-12-01 17:09:29 -0500

    added more tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,11 +1,33 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck length 16 but got", len(d))
+		t.Errorf("Expected deck length 16 but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("First card must be Ace of Spades but got %v", d[0])
+	}
+}
+
+func TestSaveToFile(t *testing.T) {
+	os.Remove("_decktest")
+
+	deck := newDeck()
+
+	deck.saveToFile("_decktest")
+
+	loadedDeck := newDeckFromFile("_decktest")
+
+	expectedCards := 16
+	if len(loadedDeck) != expectedCards {
+		t.Errorf("Expected %v cards, got %v", expectedCards, len(loadedDeck))
 	}
 }
